73ceb880fadba0574cac5fb5866af6c59ce6c688
Teresa <teresamadridejosm@gmail.com>
2023-08-13 18:21:45 +0200

    Function files added

---
+++ funciones.js
@@ -0,0 +1,21 @@
+function sumar(a, b) {
+  return a + b;
+}
+
+function restar(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  return a / b;
+}
+
+function modulo(a, b) {
+  return a % b;
+}
+
+module.exports = { sumar, restar, multiplicar, dividir, modulo };
